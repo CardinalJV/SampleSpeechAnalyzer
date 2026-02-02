0dfade94818dcc66e0f5a2a0090067957b6e7e3e
CardinalJV <129071629+CardinalJV@users.noreply.github.com>
2026-02-02 21:38:05 +0100

    Initial Commit

---
+++ SampleSpeechAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleSpeechAnalyzer
+//
+//  Created by Viranaiken Jessy on 02/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleSpeechAnalyzer/SampleSpeechAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSpeechAnalyzerApp.swift
+//  SampleSpeechAnalyzer
+//
+//  Created by Viranaiken Jessy on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleSpeechAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
